ee1c5fa441420f60e7f1fb75433089b0c92d7e1c
PlouzzY <168923795+PlouzzY@users.noreply.github.com>
2024-05-04 20:07:30 +0300

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -1,20 +1,21 @@
+# импортируем библиотеки (я работаю с disnake, есть еще discord.py)
 import disnake
 from disnake.ext import commands
 
 
-intents = disnake.Intents.all()
-bot = commands.Bot(command_prefix="=", intents=intents)
-bot.remove_command("help")
+intents = disnake.Intents.all() # определяем разрешения
+bot = commands.Bot(command_prefix="=", intents=intents) # устанавливаем префикс и разрешения
+bot.remove_command("help") # удаляем встроенную команду help 
 
 
-@bot.event
-async def on_ready():
-    print("бот запущен")
-    await bot.change_presence(activity=disnake.Game(name="игры"), status=disnake.Status.online) # активность бота, статус бота
+@bot.event # создаем ивент
+async def on_ready(): # функция срабатывает когда бот запускается
+    print("бот запущен") # получаем сообщение при запуске бота
+    await bot.change_presence(activity=disnake.Game(name="игры"), status=disnake.Status.online) # ставим активность и статус бота
 
-@bot.command(name="пинг")
-async def ping(ctx):
-    await ctx.send(f"{ctx.author.mention}, понг!")
+@bot.command(name="пинг") # создаем команду и даем название (можно оставить скобки пустыми, тогда для вызова команды упоминаем функцию, т.е - "=ping")
+async def ping(ctx): # функция команды, в скобках указываем аргументы, например - (ctx, member: disnake.Member, number: int), функция сразу понимает какой тип у аргумента должен быть
+    await ctx.send(f"{ctx.author.mention}, понг!") # отправляем сообщение (ctx.author - автор сообщения, .mention - упоминание)
 
 
-bot.run("токен")
+bot.run("токен")  # запуск бота (токен можно получить на портале разработчика)
