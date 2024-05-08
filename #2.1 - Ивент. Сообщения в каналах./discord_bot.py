1e9b57c306171e4265e9ddcb63106b5d1f1291e3
PlouzzY <168923795+PlouzzY@users.noreply.github.com>
2024-05-08 17:51:35 +0300

    Add files via upload

---
+++ #2.1 - Ивент. Сообщения в каналах./discord_bot.py
@@ -0,0 +1,30 @@
+# импортируем библиотеки (я работаю с disnake, есть еще discord.py)
+import disnake
+from disnake.ext import commands
+
+intents = disnake.Intents.all() # определяем разрешения
+bot = commands.Bot(command_prefix="=", intents=intents) # устанавливаем префикс и разрешения
+bot.remove_command("help") # удаляем встроенную команду help 
+
+
+@bot.event # создаем ивент
+async def on_ready(): # функция срабатывает когда бот запускается
+    print("бот запущен") # получаем сообщение при запуске бота
+    await bot.change_presence(activity=disnake.Game(name="игры"), status=disnake.Status.online) # ставим активность и статус бота
+
+# РЕЗУЛЬТАТ - Вид в дискорде
+@bot.event # создаем ивент
+async def on_message(message): # функция срабатывает когда кто-то отправляет сообщение
+    await bot.process_commands(message) # игнорирует команды
+    if message.author.bot: # проверка: если автор сообщения является ботом
+        return
+    if message.content == "пинг!": # проверка: если контент сообщения является ""
+        await message.channel.send("понг!") # отправляем сообщение в канал, где было получено сообщения пользователя
+
+
+
+bot.run("токен")  # запуск бота (токен можно получить на портале разработчика)
+
+
+
+
